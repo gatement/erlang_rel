3efaac5c7140ea42ca3a14da4674af67ebce4349
Johnson Lau <gatement@gmail.com>
2015-05-16 10:14:13 +0800

    go

--- README.md
+++ README.md
@@ -7,5 +7,14 @@ erlang release test
 * Customize `target/dir/bin/start`
   1. Add pipe file name
   2. Add heart monitor 
-  3. e.g $ROOTDIR/bin/run_erl -daemon `/tmp/erlang_rel` $ROOTDIR/log "exec $ROOTDIR/bin/start_erl $ROOTDIR $RELDIR $START_ERL_DATA `-heart -env HEART_COMMAND $ROOTDIR/bin/start`"
+  3. e.g $ROOTDIR/bin/run_erl -daemon `/tmp/erlang_rel.pipe` $ROOTDIR/log "exec $ROOTDIR/bin/start_erl $ROOTDIR $RELDIR $START_ERL_DATA `-heart -env HEART_COMMAND $ROOTDIR/bin/start`"
 * Use `target/dir/bin/to_erl /tmp/erlang_rel` to attach the erlang emulator 
+
+## Upgrade
+* Genetate upgrade tar by `./rel.sh upgrade from/app/root/dir to/app/root/dir`
+* Copy the generated tar file to `/usr/local/erlang_rel/releases/`
+* Attach to the erlang emulator by `target/dir/bin/to_erl /tmp/erlang_rel` 
+* Unpack by `release_handler:unpack_release("Tar_file_without_ext").`
+* List releases by `release_handler:which_releases().`
+* Install release by `release_handler:install_release("Vsn").`
+* Make permanent by `release_handler:make_permanent("Vsn").`
--- rel.sh
+++ rel.sh
@@ -12,21 +12,21 @@ elif [ "install" == "$1" -a "x" != "x$2" -a "x" != "x$3" ] ; then
     erl -pa ${SASL_DIR}/examples/ebin -eval "target_system:install(\"$2\", \"$3\")" -s init stop
 
 # make upgrade tar
-elif [ "pack" == "$1" -a "x" != "x$2" -a "x" != "x$3" ] ; then
+elif [ "upgrade" == "$1" -a "x" != "x$2" -a "x" != "x$3" ] ; then
     FROM_REL_FILE=$2/release
     TO_REL_FILE=$3/release
     # copy appup file
-    cp $2/src/*.appup $2/ebin/
+    cp $3/src/*.appup $3/ebin/
     # generate relup
     erl -pa $2 $2/ebin $2/deps/*/ebin $3 $3/ebin $3/deps/*/ebin -eval "systools:make_relup(\"$TO_REL_FILE\",[\"$FROM_REL_FILE\"],[\"$FROM_REL_FILE\"])" -s init stop
     # pack
-    erl -pa ${SASL_DIR}/examples/ebin $2/ebin $2/deps/*/ebin -eval "target_system:create(\"$TO_REL_FILE\")" -s init stop
+    erl -pa ${SASL_DIR}/examples/ebin $3/ebin $3/deps/*/ebin -eval "target_system:create(\"$TO_REL_FILE\")" -s init stop
 
 else
     echo "usage: $0 pack app/root/dir"
     echo "usage: $0 install tar/file/without/ext target/dir"
-    echo "usage: $0 pack from/app/root/dir to/app/root/dir"
+    echo "usage: $0 upgrade from/app/root/dir to/app/root/dir"
     echo "e.g make first pack  : $0 pack /app/erlang_rel"
     echo "e.g first install    : $0 install /app/release /usr/local/erlang_rel"
-    echo "e.g make upgrade pack: $0 pack /var/tmp/erlang_rel_4.0.0 /var/tmp/erlang_rel_4.0.1"
+    echo "e.g make upgrade pack: $0 upgrade /var/tmp/erlang_rel_0.1.0 /var/tmp/erlang_rel_0.2.0"
 fi
